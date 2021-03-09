74225efe115c3efbb089ab319613a631896776f1
Tanay Sheth <tanaysheth0108@gmail.com>
2021-03-09 13:38:29 +0530

    Initial sharing of project

---
+++ length.java
@@ -0,0 +1,9 @@
+//wap to accept a string,print it and print its length
+public class length
+{
+    public void display(String str)
+    {
+        System.out.println(str.length());
+        System.out.println(str);
+    }
+}
